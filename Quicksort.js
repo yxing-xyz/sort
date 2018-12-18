acd116139879560086e5de77c87ea23de25de92d
yuxing1994 <yuxing1994@126.com>
2018-12-18 23:46:33 +0800

    快排+插入排序

--- Quicksort.js
+++ Quicksort.js
@@ -3,8 +3,11 @@
     function sort(arr, leftIndex, rightIndex) {
         //定义栈
         let stack = [];
+        //定义cutoff
+        const cutoff = 10;
 
-        let node, low, hight, pivot, i, j, swap;
+
+        let node, low, hight, pivot, i, j, swap, midIndex, pivotIndex;
 
         stack.push({
             low: leftIndex,
@@ -18,49 +21,88 @@
             i = low;
             j = hight;
 
-            //三数取中
-            //let midIndex = (low + hight) >> 1;
-            pivot = arr[hight];
-
-            for (;;) {
-                while (i < j && arr[i] <= pivot) i++;
-                while (i < j && arr[j] >= pivot) j--;
-
-                if (i < j) {
-                    swap = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = swap;
-                } else {
-                    break;
+            len = hight - low;
+            if (len > cutoff) {
+                //三数取中
+                midIndex = (low + hight) >> 1;
+                if (arr[low] > arr[midIndex]) {
+                    swap = arr[low];
+                    arr[low] = arr[midIndex];
+                    arr[midIndex] = swap;
                 }
-            }
-            arr[hight] = arr[i];
-            arr[i] = pivot;
 
-            if (low < i - 1) {
-                stack.push({
-                    low: low,
-                    hight: i - 1,
-                });
-            }
+                if (arr[low] > arr[hight]) {
+                    swap = arr[low];
+                    arr[low] = arr[hight];
+                    arr[hight] = swap;
+                }
 
-            if (i + 1 < hight) {
-                stack.push({
-                    low: i + 1,
-                    hight: hight,
-                });
+                if (arr[midIndex] > arr[hight]) {
+                    swap = arr[midIndex];
+                    arr[midIndex] = arr[hight];
+                    arr[hight] = swap;
+                }
+
+                // 交换midIndex hight - 1;
+                swap = arr[midIndex];
+                arr[midIndex] = arr[hight - 1];
+                arr[hight - 1] = swap;
+
+                pivotIndex = hight - 1;
+                pivot = arr[hight - 1];
+
+
+                for (;;) {
+                    // 主意这里使用++i 和 --i 是因为三数取中区分了两端
+                    while (i < j && arr[i] <= pivot) ++i;
+                    while (i < j && arr[j] >= pivot) --j;
+                    if (i < j) {
+                        swap = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swap;
+
+                    } else {
+                        break;
+                    }
+                }
+
+                arr[pivotIndex] = arr[i];
+                arr[i] = pivot;
+
+                if (low < i - 1) {
+                    stack.push({
+                        low: low,
+                        hight: i - 1,
+                    });
+                }
+
+                if (i + 1 < hight) {
+                    stack.push({
+                        low: i + 1,
+                        hight: hight,
+                    });
+                }
+            } else {
+                // 插入排序
+                for (let i = 0; i < len; i++) {
+                    for (let j = i + low; j >= low && arr[j] > arr[j + 1]; j--) {
+                        swap = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = swap;
+                    }
+                }
             }
         }
     }
 
     // 构造数据
-    let arr = [8, 1, 4, 9, 0, 3, 5, 2, 7, 6];
-    for (let i = 0; i < 10; i += 1) {
+    //let arr = [8, 1, 4, 9, 0, 3, 5, 2, 7, 10, 13123, 1, 6];
+    let arr = [];
+    for (let i = 0; i < 10000; i += 1) {
         //arr.push(i);
-        //arr.push(10000 - i - 1);
-        //arr.push(Number.parseInt(Math.random() * 10));
+        //arr.push(1000000 - i - 1);
+        arr.push(Number.parseInt(Math.random() * 10000));
     }
-    console.log(arr);
     let start = new Date().getTime();
     sort(arr, 0, arr.length - 1);
     let end = new Date().getTime();
